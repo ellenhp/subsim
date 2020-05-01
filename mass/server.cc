ae57b5a48ee8dd1a1bf0a6a696af641b2fd655fd
Ellen Poe <ellenhp@google.com>
2020-04-30 23:53:45 -0700

    Add empty implementation of server. (#5)

---
+++ mass/server.cc
@@ -0,0 +1,42 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "server.hh"
+
+using namespace mass;
+
+grpc::Status MassBackendImpl::Connect(
+    ::grpc::ServerContext *context,
+    grpc::ServerReaderWriter<MassUpdate, MassRequest> *stream)
+{
+  return grpc::Status::OK;
+}
+
+MassServer::MassServer(std::string server_address_and_port)
+{
+  _server_address_and_port = server_address_and_port;
+}
+
+void MassServer::run_server_forever()
+{
+  MassBackendImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(_server_address_and_port, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  server->Wait();
+}
---
+++ mass/server.hh
@@ -0,0 +1,45 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "mass/api/mass.grpc.pb.h"
+
+namespace mass
+{
+
+class MassBackendImpl final : public MassBackend::Service
+{
+public:
+  grpc::Status Connect(
+      grpc::ServerContext *context,
+      grpc::ServerReaderWriter<MassUpdate, MassRequest> *stream);
+};
+
+class MassServer
+{
+public:
+  MassServer(std::string server_address_and_port);
+
+  void run_server_forever();
+
+private:
+  std::string _server_address_and_port;
+};
+
+} // namespace mass
