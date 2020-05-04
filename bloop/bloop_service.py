f412e13111bba37ce875c1466e52756e10ad3c97
Ellen Poe <ellenhp@google.com>
2020-05-03 22:00:05 -0700

    Add a performance test to bloop

--- bloop/bloop_service.py
+++ bloop/bloop_service.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MASS.  If not, see <http://www.gnu.org/licenses/>.
 
-from bloop.api import bloop_pb2_grpc
+from bloop.api import bloop_pb2_grpc, bloop_pb2
 import grpc
 import concurrent.futures as futures
 import arlpy.uwapm as pm
@@ -24,20 +24,31 @@ import numpy as np
 class BloopServicer(bloop_pb2_grpc.BloopServicer):
   def Propagate(self, request, context):
     bathymetry = [[point.range_meters, point.depth_meters] for point in request.bathymetry.points]
-    print(bathymetry)
     env = pm.create_env2d(rx_range=np.asarray(request.ranges),
                           rx_depth=np.asarray(request.depths),
-                          # depth=bathymetry,
+                          depth=bathymetry,
                           frequency=request.frequency)
-    # env = pm.create_env2d(rx_range=100, frequency=200)
-    tloss = pm.compute_transmission_loss(env, mode=pm.incoherent)
-    print(tloss)
-    print(request)
+    loss = pm.compute_transmission_loss(env, mode=pm.incoherent).to_numpy()
+    response = bloop_pb2.PropagateResponse()
+    depth_index = 0
+    for row in loss:
+      depth = request.depths[depth_index]
+
+      row_index = 0
+      for r in row:
+        point = bloop_pb2.PropagationLossGrid.PropagationLossPoint()
+        point.depth_meters = depth
+        point.range_meters = request.ranges[row_index]
+        point.loss = np.absolute(r)
+        response.loss.points.append(point)
+        row_index += 1
+      depth_index += 1
+    return response
 
 def start(should_wait = True):
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=30))
   bloop_pb2_grpc.add_BloopServicer_to_server(BloopServicer(), server)
-  server.add_insecure_port('[::]:50051')
+  server.add_insecure_port('0.0.0.0:50051')
   server.start()
   if (should_wait):
     server.wait_for_termination()
--- bloop/bloop_service_test.py
+++ bloop/bloop_service_test.py
@@ -18,27 +18,46 @@
 from bloop_service import start
 from bloop.api import bloop_pb2_grpc, bloop_pb2
 import grpc
+from multiprocessing import Process
 
-print("Starting server")
-start(False)
-print("Started server")
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = bloop_pb2_grpc.BloopStub(channel)
+def start_server():
+  print("Starting server")
+  p = Process(target=start, args=('False',))
+  p.start()
+  print("Started server")
+  return p
 
-print()
-request = bloop_pb2.PropagateRequest()
+def make_request():
+  request = bloop_pb2.PropagateRequest()
 
-request.ranges.append(100)
-request.depths.append(10)
+  request.ranges.append(100)
+  request.ranges.append(200)
+  request.depths.append(10)
 
-bathymetry = bloop_pb2.BathymetricProfile()
-for i in [0, 20000]:
-  point = bloop_pb2.BathymetricProfile.BathymetricProfilePoint()
-  point.depth_meters = 25
-  point.range_meters = i
-  request.bathymetry.points.append(point)
+  bathymetry = bloop_pb2.BathymetricProfile()
+  for i in [-1, 20000]: # For some reason there has to be a point with negative range.
+    point = bloop_pb2.BathymetricProfile.BathymetricProfilePoint()
+    point.depth_meters = 25
+    point.range_meters = i
+    request.bathymetry.points.append(point)
 
-request.frequency = 200
+  request.frequency = 200
+  return request
 
-feature = stub.Propagate(request)
+def get_stub():
+  channel = grpc.insecure_channel('0.0.0.0:50051')
+  stub = bloop_pb2_grpc.BloopStub(channel)
+  return stub
+
+process = start_server()
+stub = get_stub()
+
+import timeit
+times = 5
+t = timeit.timeit('stub.Propagate(make_request())', '''
+from __main__ import get_stub, make_request, start_server
+stub=get_stub()
+''', number=times)
+print('RPC time: {}'.format(t/times))
+process.terminate()
