a94882702120fd24383a99153444593c723a6226
Evin Sellin <evinism@gmail.com>
2020-05-09 16:20:40 -0700

    lotsa changes to get rollup working

--- horrificlient/rollup.config.js
+++ horrificlient/rollup.config.js
@@ -1,11 +1,20 @@
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
+  preferBuiltins: false,
   input: "./main.js",
+  context: "window",
   output: {
     format: "iife",
-    file: "output/bundle.js",
+    file: "bundle.js",
   },
-  //external: ["crypto"],
-  plugins: [resolve()],
+  plugins: [
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: "node_modules",
+      },
+    }),
+    commonjs(), // converts date-fns to ES modules
+  ],
 };
