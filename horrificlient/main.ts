6e613badd19bdbea593e9cc7f0aa97598b80047c
Ellen Poe <ellenhp@google.com>
2020-05-08 18:00:38 -0700

    Maybe connect?

--- horrificlient/main.ts
+++ horrificlient/main.ts
@@ -1,6 +1,6 @@
 import * as mass_grpc_web_pb from '../mass/api/mass_grpc_web_pb';
+import * as mass_pb from '../mass/api/mass_pb';
 
-var client = new mass_grpc_web_pb.MassBackendPromiseClient('hostname')
-
-console.log('lololol');
-console.log(client);
+var client = new mass_grpc_web_pb.MassBackendPromiseClient('subsim.io')
+var request = new mass_pb.ConnectRequest()
+console.log(client.connect(request))
