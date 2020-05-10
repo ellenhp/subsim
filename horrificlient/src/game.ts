71894f074357d2fdbff87f53aca0be41b47ae500
Evin Sellin <evinism@gmail.com>
2020-05-09 17:34:20 -0700

    progress

--- horrificlient/src/game.ts
+++ horrificlient/src/game.ts
@@ -1,20 +1,45 @@
-import * as mass_grpc_web from "../../mass/api/mass_grpc_web_pb";
-import * as mass_pb from "../../mass/api/mass_pb";
+import { MassBackendPromiseClient } from "../../mass/api/mass_grpc_web_pb";
+import { ConnectRequest } from "../../mass/api/mass_pb";
+import { VesselUpdate } from "../../mass/api/updates_pb";
 import { Pipe } from "./util";
 import { v4 } from "uuid";
 
-const client = new mass_grpc_web.MassBackendPromiseClient("http://subsim.io");
-const request = new mass_pb.ConnectRequest();
-client.connect(request);
+const client = new MassBackendPromiseClient("http://subsim.io");
 
 interface Game {
-  gameId: string;
+  scenarioId: string;
   vesselId: string;
-  worldEvents: Pipe<any>;
+  worldEvents: Pipe<VesselUpdate>;
   performAction: (action: any) => (response: any) => unknown;
 }
 
-function createNewGame() {
-  const gameId = v4();
+function createNewGame(): Game {
+  const scenarioId = v4();
   const vesselId = v4();
+
+  const connectionReq = new ConnectRequest();
+  connectionReq.setVesselUniqueId(vesselId);
+  connectionReq.setScenarioId(scenarioId);
+
+  const worldEvents = new Pipe<VesselUpdate>();
+
+  const request = new ConnectRequest();
+  // The reason we're wrapping this in a pipe is so that
+  // we can reconnect if necessary...
+  client.connect(request).then((response: VesselUpdate) => {
+    worldEvents.fire(response);
+  });
+
+  function performAction() {
+    client.
+  }
+
+  return {
+    scenarioId,
+    vesselId,
+    worldEvents,
+    performAction,
+  };
 }
+
+createNewGame();
