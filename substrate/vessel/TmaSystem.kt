6f0042b7c1a777147a9e29293afd50e22b9b4d5b
Ellen Poe <ellenhp@google.com>
2020-05-14 16:37:29 -0700

    Contact management and bearings are working

--- substrate/vessel/TmaSystem.kt
+++ substrate/vessel/TmaSystem.kt
@@ -1,4 +1,83 @@
 package substrate.vessel
 
-class TmaSystem {
+import api.Actions
+import api.Systems
+import api.Updates
+import java.time.Instant
+import java.util.concurrent.locks.Lock
+
+class TmaSystem(vessel: Vessel, val descriptor: Systems.TmaSystem) : VesselSystem(vessel) {
+
+    private val lock = Any()
+    private val contacts = HashMap<String, Updates.TmaSystemUpdate.TmaContact>()
+    private var nextSonarDesignationNumber = 1
+    private var nextMergeDesignationNumber = 1
+
+    override fun getSystemUpdate(): Updates.SystemUpdate {
+        synchronized(lock) {
+            return Updates.SystemUpdate.newBuilder().setTmaUpdate(
+                    Updates.TmaSystemUpdate.newBuilder()
+                            .addAllContacts(contacts.values)
+            ).build()
+        }
+    }
+
+    override fun processRequest(request: Actions.SystemRequest) {
+        if (!request.hasTmaRequest()) {
+            return
+        }
+        val tmaRequest = request.tmaRequest
+        when {
+            tmaRequest.hasAddContactRequest() -> addContact(tmaRequest.addContactRequest)
+            tmaRequest.hasTakeBearingRequest() -> takeBearing(tmaRequest.takeBearingRequest)
+            tmaRequest.hasMergeContactRequest() -> mergeContact(tmaRequest.mergeContactRequest)
+            tmaRequest.hasDeleteContactRequest() -> deleteContact(tmaRequest.deleteContactRequest)
+        }
+    }
+
+    private fun addContact(addContactRequest: Actions.TmaSystemRequest.TmaAddContactSubrequest) {
+        synchronized(lock) {
+            val designation = "S$nextSonarDesignationNumber"
+            nextSonarDesignationNumber += 1
+            contacts[designation] = Updates.TmaSystemUpdate.TmaContact.newBuilder()
+                    .setDesignation(designation)
+                    .build()
+        }
+    }
+
+    private fun takeBearing(request: Actions.TmaSystemRequest.TmaTakeBearingSubrequest) {
+        synchronized(lock) {
+            val builder = contacts[request.designation]?.toBuilder()
+                    ?: throw NoSuchContactException("Expected to find contact with designation ${request.designation}")
+            builder.addBearings(Updates.TmaSystemUpdate.TmaContact.Bearing.newBuilder()
+                    .setBearingDegrees(request.bearingDegrees)
+                    .setEpochMillis(Instant.now().toEpochMilli())
+                    .setLocation(vessel.position))
+            contacts[request.designation] = builder.build()
+        }
+    }
+
+    private fun mergeContact(mergeContactRequest: Actions.TmaSystemRequest.TmaMergeContactSubrequest) {
+        synchronized(lock) {
+            val designation = "M$nextMergeDesignationNumber"
+            nextMergeDesignationNumber += 1
+            val mergedContactBuilder = Updates.TmaSystemUpdate.TmaContact.newBuilder()
+                    .setDesignation(designation)
+            for (i in 0 until mergeContactRequest.designationsCount) {
+                mergedContactBuilder.addAllBearings(
+                        contacts[mergeContactRequest.getDesignations(i)]?.bearingsList
+                                ?: throw NoSuchContactException("Expected to find contact ${mergeContactRequest.getDesignations(i)}"))
+            }
+            for (i in 0 until mergeContactRequest.designationsCount) {
+                contacts.remove(mergeContactRequest.getDesignations(i))
+            }
+            contacts[designation] = mergedContactBuilder.build()
+        }
+    }
+
+    private fun deleteContact(deleteContactRequest: Actions.TmaSystemRequest.TmaDeleteContactSubrequest) {
+        synchronized(lock) {
+            contacts.remove(deleteContactRequest.designation)
+        }
+    }
 }
