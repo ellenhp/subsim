364baadf5604ff3714d59175dfcb6eab7b31bfc2
Ellen Poe <ellenhp@google.com>
2020-05-14 23:21:23 -0700

    TMA solutions are automatically moved to the map (#55)

--- substrate/vessel/PropulsionSystem.kt
+++ substrate/vessel/PropulsionSystem.kt
@@ -1,6 +1,7 @@
 package substrate.vessel
 
 import api.*
+import substrate.utils.Utils.Companion.stepPosition
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 import java.time.temporal.TemporalAmount
@@ -42,27 +43,12 @@ class PropulsionSystem(vessel: Vessel, val descriptor: Systems.PropulsionSystem)
 
     private fun updatePosition(dt: Duration) {
         val position = vessel.position
-        // We'll need this later, compute it now for clarity :)
-        val absoluteLatitudeRadians = position.lat / 180 * Math.PI
+        val newPosition = stepPosition(
+                position = position,
+                speedKnots = actualSpeedKnots,
+                heading = vessel.heading,
+                dt = dt)
 
-        // Determine the components of the velocity in a locally cartesian X-Y
-        // grid, in knots.
-        val headingRadians = vessel.heading / 180 * Math.PI
-        // A heading of zero is due north, so we need to use sin for X and cos for Y.
-        val xKnots = actualSpeedKnots * sin(headingRadians)
-        val yKnots = actualSpeedKnots * cos(headingRadians)
-
-        @Suppress("UnnecessaryVariable")
-        // One knot is 1 nmi / hr, which approximates to one minute of latitude / hr.
-        val latMinutesPerHour = yKnots;
-        // Longitudinal speed depends on our absolute latitude.
-        val lngMinutesPerHour = xKnots / cos(absoluteLatitudeRadians)
-
-        val dtHours = dt.toNanos().toDouble() / Duration.ofHours(1).toNanos()
-        val newPosition = Spatial.Position.newBuilder()
-                .setLat(position.lat + dtHours * latMinutesPerHour / 60)
-                .setLng(position.lng + dtHours * lngMinutesPerHour / 60)
-                .build()
 
         vessel.position = newPosition
     }
