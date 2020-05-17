de67e6408b630613f5b9a5535007f7902c2dd770
Ellen Poe <ellenhp@google.com>
2020-05-16 20:40:43 -0700

    Finish up final scenario modulo bugs/testing

--- cowfarts/src/builders/feasibleScenario.ts
+++ cowfarts/src/builders/feasibleScenario.ts
@@ -5,43 +5,119 @@ import {ConnectRequest} from '../__protogen__/mass/api/mass_pb';
 import {MassBackendClient} from '../__protogen__/mass/api/MassServiceClientPb';
 import {EndCondition, Faction, Scenario, SpawnedVessel, VesselDescriptor,} from '../__protogen__/mass/api/scenario_pb';
 import {Bounds, HeadingBounds, Position,} from '../__protogen__/mass/api/spatial_pb';
-import {DivingSystem, HullSystem, MapSystem, PropulsionSystem, SonarSystem, SteeringSystem, VesselSystem,} from '../__protogen__/mass/api/systems_pb';
+import {DivingSystem, GuidanceSystem, HullSystem, MapSystem, PropulsionSystem, SelfDestructSystem, SonarSystem, SteeringSystem, VesselSystem, WeaponSystem,} from '../__protogen__/mass/api/systems_pb';
 import {VesselUpdate} from '../__protogen__/mass/api/updates_pb';
+import {Armament, Weapon,} from '../__protogen__/mass/api/weapons_pb';
 import {Pipe} from '../util/pipe';
 
-function buildNewFeasibleScenario(playerId: string): Scenario {
-  const submarineDescriptorId = v4();
+function buildNewFeasibleScenario(): Scenario {
+  const submarineDescriptorId = 'submarine';
 
-  // Systems for the player
+  const rebelFaction = new Faction();
+  rebelFaction.setPlayerControlled(true);
+  rebelFaction.setName('Rebels');
 
+  const blockaderFaction = new Faction();
+  blockaderFaction.setPlayerControlled(true);
+  blockaderFaction.setName('Blockaders');
+
+  const scenario = new Scenario();
+  scenario.addVesselDescriptors(getSubmarineDescriptor(submarineDescriptorId));
+  scenario.addVesselDescriptors(getTorpedoDescriptor());
+  scenario.addVesselDescriptors(getNoisemakerDescriptor());
+  scenario.addVesselDescriptors(getDecoyDescriptor());
+  scenario.addVessels(
+      getRebelVessel(rebelFaction, submarineDescriptorId, 'rebels'));
+  scenario.addVessels(getBlockaderVessel(
+      blockaderFaction, submarineDescriptorId, 'blockaders'));
+
+  return scenario;
+}
+
+function getRebelVessel(
+    faction: Faction, submarineDescriptorId: string, vesselId: string) {
+  const rebelSpawnPosition = new Position();
+  rebelSpawnPosition.setLat(47.723117693);
+  rebelSpawnPosition.setLng(-122.52533117);
+
+  const rebelSpawnInfo = new SpawnedVessel.SpawnInformation();
+  rebelSpawnInfo.setPosition(rebelSpawnPosition);
+  rebelSpawnInfo.setExactSpawnHeading(60);
+
+  const rebelVessel = new SpawnedVessel();
+  rebelVessel.setVesselDescriptorId(submarineDescriptorId);
+  rebelVessel.setUniqueId(vesselId);
+  rebelVessel.setSpawnInfo(rebelSpawnInfo);
+  rebelVessel.setFaction(faction);
+
+  return rebelVessel
+}
+
+function getBlockaderVessel(
+    faction: Faction, submarineDescriptorId: string, vesselId: string) {
+  const blockaderPos = new Position();
+  blockaderPos.setLat(47.938689);
+  blockaderPos.setLng(-122.547806);
+
+  const blockaderSpawnInfo = new SpawnedVessel.SpawnInformation();
+  blockaderSpawnInfo.setPosition(blockaderPos);
+  blockaderSpawnInfo.setExactSpawnHeading(100);
+
+
+  const blockaderVessel = new SpawnedVessel();
+  blockaderVessel.setVesselDescriptorId(submarineDescriptorId);
+  blockaderVessel.setUniqueId(vesselId);
+  blockaderVessel.setSpawnInfo(blockaderSpawnInfo);
+  blockaderVessel.setFaction(faction);
+
+  return blockaderVessel
+}
+
+function getSubmarineSteeringSystem(): VesselSystem {
   const steering = new SteeringSystem();
   steering.setDegreesPerSecond(4);
   const steeringSystem = new VesselSystem();
   steeringSystem.setSteeringSystem(steering);
+  return steeringSystem;
+}
 
+function getSubmarineDivingSystem(): VesselSystem {
   const diving = new DivingSystem();
   diving.setFeetPerSecond(3);
   diving.setMaxDepthFeet(1000);
   const divingSystem = new VesselSystem();
   divingSystem.setDivingSystem(diving);
+  return divingSystem;
+}
 
+function getSubmarinePropulsionSystem(): VesselSystem {
   const propulsion = new PropulsionSystem();
-  propulsion.setKnotsPerSecond(1);
+  propulsion.setKnotsPerSecond(2);
   propulsion.setMaxSpeedKnots(33);
   propulsion.setBaseNoisePower(10);
   propulsion.setNoisePerKnotNoncavitating(2);
   const propulsionSystem = new VesselSystem();
   propulsionSystem.setPropulsionSystem(propulsion);
+  return propulsionSystem;
+}
 
+function getGenericMapSystem(): VesselSystem {
   const map = new MapSystem();
   const mapSystem = new VesselSystem();
   mapSystem.setMapSystem(map);
+  return mapSystem;
+}
 
+function getSubmarineHullSystem(): VesselSystem {
   const hull = new HullSystem();
   hull.setDraftFeet(31);
+  hull.setSonarReturn(1);
   const hullSystem = new VesselSystem();
   hullSystem.setHullSystem(hull);
+  return hullSystem;
+}
 
+function getSubmarineSonarSystem(): VesselSystem {
   const sonarArray = new SonarSystem.SonarArray();
   sonarArray.setUniqueId('fore');
   sonarArray.setNoiseFloor(0);
@@ -50,115 +126,183 @@ function buildNewFeasibleScenario(playerId: string): Scenario {
   sonar.addSonarArrays(sonarArray);
   const sonarSystem = new VesselSystem();
   sonarSystem.setSonarSystem(sonar);
+  return sonarSystem;
+}
 
+function getSubmarineWeaponSystem(): VesselSystem {
+  const torpoedoVesselDescriptorId = 'adcap';
+  const noisemakerVesselDescriptorId = 'noisemaker';
+  const decoyVesselDescriptorId = 'decoy';
+
+  const torpedo = new Weapon();
+  torpedo.setName('ADCAP');
+  torpedo.setType(Weapon.AmmoType.TORPEDO);
+  torpedo.setWeaponVesselDescriptor(torpoedoVesselDescriptorId);
+
+  const torpedoArmament = new Armament();
+  torpedoArmament.setWeapon(torpedo);
+  torpedoArmament.setCount(4);
+
+  const noisemaker = new Weapon();
+  noisemaker.setName('Noisemaker');
+  noisemaker.setType(Weapon.AmmoType.COUNTERMEASURE);
+  noisemaker.setWeaponVesselDescriptor(noisemakerVesselDescriptorId);
+
+  const noisemakerArmament = new Armament();
+  noisemakerArmament.setWeapon(noisemaker);
+  noisemakerArmament.setCount(4);
+
+  const decoy = new Weapon();
+  decoy.setName('Decoy');
+  decoy.setType(Weapon.AmmoType.COUNTERMEASURE);
+  decoy.setWeaponVesselDescriptor(decoyVesselDescriptorId);
+
+  const decoyArmament = new Armament();
+  decoyArmament.setWeapon(decoy);
+  decoyArmament.setCount(4);
+
+  const weapon = new WeaponSystem();
+  weapon.addArmament(torpedoArmament);
+  weapon.addArmament(noisemakerArmament);
+  weapon.addArmament(decoyArmament);
+  const weaponSystem = new VesselSystem();
+  weaponSystem.setWeaponSystem(weapon);
+  return weaponSystem;
+}
+
+function getSubmarineDescriptor(submarineDescriptorId: string):
+    VesselDescriptor {
   const submarine = new VesselDescriptor();
   submarine.setUniqueId(submarineDescriptorId);
   submarine.setType(2);
-  submarine.addSystems(steeringSystem);
-  submarine.addSystems(divingSystem);
-  submarine.addSystems(propulsionSystem);
-  submarine.addSystems(mapSystem);
-  submarine.addSystems(hullSystem);
-  submarine.addSystems(sonarSystem);
+  submarine.addSystems(getSubmarineSteeringSystem());
+  submarine.addSystems(getSubmarineDivingSystem());
+  submarine.addSystems(getSubmarinePropulsionSystem());
+  submarine.addSystems(getGenericMapSystem());
+  submarine.addSystems(getSubmarineHullSystem());
+  submarine.addSystems(getSubmarineSonarSystem());
+  submarine.addSystems(getSubmarineWeaponSystem());
 
-  const playerSpawn = new Position();
-  playerSpawn.setLat(47.603);
-  playerSpawn.setLng(-122.374);
+  return submarine;
+}
 
-  const playerSpawnInfo = new SpawnedVessel.SpawnInformation();
-  playerSpawnInfo.setPosition(playerSpawn);
-  playerSpawnInfo.setExactSpawnHeading(270);
+function getTorpedoPropulsionSystem(): VesselSystem {
+  const propulsion = new PropulsionSystem();
+  propulsion.setKnotsPerSecond(4);
+  propulsion.setMaxSpeedKnots(55);
+  propulsion.setBaseNoisePower(10);
+  propulsion.setNoisePerKnotNoncavitating(2);
+  const propulsionSystem = new VesselSystem();
+  propulsionSystem.setPropulsionSystem(propulsion);
+  return propulsionSystem;
+}
 
-  const headingBounds = new HeadingBounds();
-  headingBounds.setLeftBound(10);
-  headingBounds.setLeftBound(11);
+function getTorpedoSteeringSystem(): VesselSystem {
+  const steering = new SteeringSystem();
+  steering.setDegreesPerSecond(2);
+  const steeringSystem = new VesselSystem();
+  steeringSystem.setSteeringSystem(steering);
+  return steeringSystem;
+}
 
-  // Building Enemy Spawn info (NEAR)
-  const enemyNePos = new Position();
-  enemyNePos.setLat(47.632545);
-  enemyNePos.setLng(-122.474061);
+function getTorpedoGuidanceSystem(): VesselSystem {
+  const guidance = new GuidanceSystem();
+  const guidanceSystem = new VesselSystem();
+  guidanceSystem.setGuidanceSystem(guidance);
+  return guidanceSystem;
+}
 
-  const enemySwPos = new Position();
-  enemySwPos.setLat(47.581621);
-  enemySwPos.setLng(-122.417695);
+function getTorpedoSelfDestructSystem(): VesselSystem {
+  const selfDestruct = new SelfDestructSystem();
+  selfDestruct.setKillRadiusFeet(150);
+  selfDestruct.setTriggerRadiusFeet(150);
+  const selfDestructSystem = new VesselSystem();
+  selfDestructSystem.setSelfDestructSystem(selfDestruct);
+  return selfDestructSystem;
+}
 
-  const enemySpawnBounds = new Bounds();
-  enemySpawnBounds.setNorthEast(enemyNePos);
-  enemySpawnBounds.setSouthWest(enemySwPos);
+function getTorpedoHullSystem(): VesselSystem {
+  const hull = new HullSystem();
+  // Draft is actually just minimum depth, and since torpedoes don't have diving
+  // systems yet, let's set this to something more than it actually is.
+  hull.setDraftFeet(15);
+  const hullSystem = new VesselSystem();
+  hullSystem.setHullSystem(hull);
+  return hullSystem;
+}
 
-  const enemySpawnInfo = new SpawnedVessel.SpawnInformation();
-  enemySpawnInfo.setBounds(enemySpawnBounds);
-  enemySpawnInfo.setHeadingBounds(headingBounds);
+function getTorpedoDescriptor() {
+  const torpedo = new VesselDescriptor();
+  torpedo.setUniqueId('adcap');
+  torpedo.addSystems(getTorpedoPropulsionSystem());
+  torpedo.addSystems(getTorpedoSteeringSystem());
+  torpedo.addSystems(getTorpedoGuidanceSystem());
+  torpedo.addSystems(getTorpedoSelfDestructSystem());
+  torpedo.addSystems(getTorpedoHullSystem());
+  // Torpedoes don't need to dive right now.
+  return torpedo;
+}
 
-  // Building Enemy Spawn Info (FAR + QUIET)
-  const enemyFarNePos = new Position();
-  enemyFarNePos.setLat(47.897931);
-  enemyFarNePos.setLng(-122.478928);
+function getNoisemakerPropulsionSystem(): VesselSystem {
+  const propulsion = new PropulsionSystem();
+  propulsion.setMaxSpeedKnots(0);
+  propulsion.setBaseNoisePower(30);
+  const propulsionSystem = new VesselSystem();
+  propulsionSystem.setPropulsionSystem(propulsion);
+  return propulsionSystem;
+}
 
-  const enemyFarSwPos = new Position();
-  enemyFarSwPos.setLat(47.839432);
-  enemyFarSwPos.setLng(-122.390979);
+function getNoisemakerSelfDestructSystem(): VesselSystem {
+  const selfDestruct = new SelfDestructSystem();
+  // 15 minutes of fun!
+  selfDestruct.setSelfDestructTimerSeconds(900);
+  const selfDestructSystem = new VesselSystem();
+  selfDestructSystem.setSelfDestructSystem(selfDestruct);
+  return selfDestructSystem;
+}
 
-  const enemyFarSpawnBounds = new Bounds();
-  enemyFarSpawnBounds.setNorthEast(enemyFarNePos);
-  enemyFarSpawnBounds.setSouthWest(enemyFarSwPos);
+function getNoisemakerHullSystem(): VesselSystem {
+  const hull = new HullSystem();
+  hull.setDraftFeet(15);
+  hull.setSonarReturn(0.1);
+  const hullSystem = new VesselSystem();
+  hullSystem.setHullSystem(hull);
+  return hullSystem;
+}
 
-  const enemyFarSpawnInfo = new SpawnedVessel.SpawnInformation();
-  enemyFarSpawnInfo.setBounds(enemyFarSpawnBounds);
-  enemyFarSpawnInfo.setHeadingBounds(headingBounds);
+function getNoisemakerDescriptor() {
+  const noisemaker = new VesselDescriptor();
+  noisemaker.setUniqueId('noisemaker');
+  noisemaker.addSystems(getNoisemakerPropulsionSystem());
+  noisemaker.addSystems(getNoisemakerSelfDestructSystem());
+  noisemaker.addSystems(getNoisemakerHullSystem());
+  return noisemaker;
+}
 
-  const playerFaction = new Faction();
-  playerFaction.setPlayerControlled(true);
-  playerFaction.setName('player');
+function getDecoySelfDestructSystem(): VesselSystem {
+  const selfDestruct = new SelfDestructSystem();
+  // 15 minutes of fun!
+  selfDestruct.setSelfDestructTimerSeconds(900);
+  const selfDestructSystem = new VesselSystem();
+  selfDestructSystem.setSelfDestructSystem(selfDestruct);
+  return selfDestructSystem;
+}
 
-  const enemyFaction = new Faction();
-  enemyFaction.setPlayerControlled(true);
-  enemyFaction.setName('enemy');
+function getDecoyHullSystem(): VesselSystem {
+  const hull = new HullSystem();
+  hull.setDraftFeet(15);
+  hull.setSonarReturn(1.5);
+  const hullSystem = new VesselSystem();
+  hullSystem.setHullSystem(hull);
+  return hullSystem;
+}
 
-  const playerVessel = new SpawnedVessel();
-  playerVessel.setVesselDescriptorId(vesselId);
-  playerVessel.setUniqueId(playerId);
-  playerVessel.setSpawnInfo(playerSpawnInfo);
-  playerVessel.setFaction(playerFaction);
-
-  const enemyVessel = new SpawnedVessel();
-  enemyVessel.setVesselDescriptorId(vesselId);
-  enemyVessel.setUniqueId(v4());
-  enemyVessel.setSpawnInfo(enemySpawnInfo);
-  enemyVessel.setFaction(enemyFaction);
-
-  // Far one!
-  const enemyFarVessel = new SpawnedVessel();
-  enemyFarVessel.setVesselDescriptorId(vesselId);
-  enemyFarVessel.setUniqueId(v4());
-  enemyFarVessel.setSpawnInfo(enemyFarSpawnInfo);
-  enemyFarVessel.setFaction(enemyFaction);
-
-  const endCondition = new EndCondition();
-  endCondition.setFactionEliminated(playerFaction);
-  endCondition.setWinningFaction(enemyFaction);
-
-  const scnNePos = new Position();
-  scnNePos.setLat(48.1900463);
-  scnNePos.setLng(-123.1800463);
-
-  const scnSwPos = new Position();
-  scnSwPos.setLat(47.0099536);
-  scnSwPos.setLng(-122.1601388);
-
-  const scnBounds = new Bounds();
-  scnBounds.setNorthEast(scnNePos);
-  scnBounds.setSouthWest(scnSwPos);
-
-  const scenario = new Scenario();
-  scenario.addVesselDescriptors(vesselDescriptor);
-  scenario.addVessels(playerVessel);
-  scenario.addVessels(enemyVessel);
-  scenario.addVessels(enemyFarVessel);
-  scenario.addEndConditions(endCondition);
-  scenario.setScenarioBounds(scnBounds);
-
-  return scenario;
+function getDecoyDescriptor() {
+  const torpedo = new VesselDescriptor();
+  torpedo.setUniqueId('decoy');
+  torpedo.addSystems(getDecoySelfDestructSystem());
+  torpedo.addSystems(getDecoyHullSystem());
+  return torpedo;
 }
 
 export default buildNewFeasibleScenario;
